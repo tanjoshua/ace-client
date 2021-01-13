9ec28d7a7c3a2dd234c0a775aa804f844804a383
tanjoshua <jtanjoshua@gmail.com>
2021-01-13 18:23:59 -0500

    browse search

--- screens/listings/Browse.js
+++ screens/listings/Browse.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const browse = () => {
+  [searchQuery, setSearchQuery] = useState("");
+
   return (
-    <View>
-      <Text>browse</Text>
-    </View>
+    <SafeAreaView style={styles.screen}>
+      <Searchbar placeholder="Search" value={searchQuery} />
+      <View style={styles.categories}>
+        <Text>browse</Text>
+      </View>
+      <View style={styles.latest}>
+        <Text>latest</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default browse;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: { flex: 1, alignItems: "center", padding: 20 },
+  categories: { paddingVertical: 10 },
+  latest: { paddingVertical: 10 },
+});
