bfb70f0c86566a88457c3ebaf582a78ed16279d8
tanjoshua <jtanjoshua@gmail.com>
2021-01-13 07:53:56 -0500

    login page

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Headline, Title } from "react-native-paper";
+import { Button, Headline, TextInput, Title } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const login = () => {
   return (
     <SafeAreaView style={styles.screen}>
       <Headline>Welcome to Ace</Headline>
-      <View></View>
+      <View style={styles.login}>
+        <TextInput label="Email" mode="outlined" />
+        <TextInput label="Password" mode="outlined" secureTextEntry={true} />
+        <Button style={styles.forgotPassword}>Forgot Password?</Button>
+      </View>
+      <View style={styles.buttons}>
+        <Button mode="contained">Login</Button>
+        <Button>Create Account</Button>
+      </View>
     </SafeAreaView>
   );
 };
@@ -16,6 +24,14 @@ export default login;
 
 const styles = StyleSheet.create({
   screen: {
+    flex: 1,
     padding: 20,
+    justifyContent: "space-between",
+  },
+  login: {
+    paddingVertical: 10,
+  },
+  forgotPassword: {
+    alignSelf: "flex-end",
   },
 });
